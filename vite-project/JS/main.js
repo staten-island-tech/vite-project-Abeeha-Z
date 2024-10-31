bcd65519af384c49ef8adb169558cc80542f6475
Abeeha-Z <158211569+Abeeha-Z@users.noreply.github.com>
2024-10-31 13:58:43 -0400

    regtr

--- vite-project/JS/main.js
+++ vite-project/JS/main.js
@@ -1,3 +1,6 @@
-import '../products'
-import"../vite-project/
+import "../CSS/style.css";
+import { coffeeShopMenu } from "./products";
 
+document.querySelector(coffeeShopMenu.coffees.latte).innerHTML = `
+    <h3>Hello</h3>
+`;
