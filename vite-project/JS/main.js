626fc1c46193abd2f4794284c6b7b4db3136e6a9
Abeeha-Z <abeehaz@nycstudents.net>
2024-11-11 15:56:29 -0500

    ght

--- vite-project/JS/main.js
+++ vite-project/JS/main.js
@@ -21,4 +21,21 @@ function createCards() {
   });
 }
 
+//theme toggle button
+function switchtheme() {
+  document.querySelector(".themebtn").addEventListener("click", function () {
+    if (document.body.classList.contains("light")) {
+      document.body.classList.remove("light");
+      document.body.classList.add("dark");
+    } else {
+      document.body.classList.remove("dark");
+      document.body.classList.add("light");
+    }
+  });
+}
+
+//select button
+//filter
+
 createCards();
+switchtheme();
